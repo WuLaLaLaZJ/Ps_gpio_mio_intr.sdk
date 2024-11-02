ddc8c8ac20fcaec3b8d0c419dfd5ce27dfc4b3ce
WuLaLaLaZJ <2474380008@qq.com>
2024-11-02 16:01:31 +0800

    FPGA的PS端，用MIO实现中断功能

---
+++ GPIO_MIO_intr_bsp_xadcps_polled_printf_example_1/Debug/src/xadcps_polled_printf_example.d
@@ -0,0 +1,50 @@
+src/xadcps_polled_printf_example.o src/xadcps_polled_printf_example.o: \
+ ../src/xadcps_polled_printf_example.c \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xadcps.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xadcps_hw.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_printf.h
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xadcps.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xadcps_hw.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_printf.h:
---
+++ GPIO_MIO_intr_bsp_xgpiops_intr_example_1/Debug/src/xgpiops_intr_example.d
@@ -0,0 +1,65 @@
+src/xgpiops_intr_example.o src/xgpiops_intr_example.o: \
+ ../src/xgpiops_intr_example.c \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xgpiops.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xgpiops_hw.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_printf.h
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xgpiops.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xgpiops_hw.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../GPIO_MIO_intr_bsp/ps7_cortexa9_0/include/xil_printf.h:
